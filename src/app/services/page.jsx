77a20798c8460160ca6f86fd64f9557848ec5211
Mohamed <mohamed.warma10@gmail.com>
2025-02-15 02:07:53 +0000

    change of service page

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,7 +1,70 @@
+'use client'
+
+import { BsArrowDownRight } from 'react-icons/bs'
+import {motion} from 'framer-motion'
+import Link from 'next/link'
+
+const services = [
+  {
+    num:'01',
+    title:'web development',
+    description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, reiciendis!',
+    href:''
+  },
+  {
+    num:'02',
+    title:'Mobile development',
+    description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, reiciendis!',
+    href:''
+  },
+  {
+    num:'03',
+    title:'Cloud computing',
+    description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, reiciendis!',
+    href:''
+  },
+  {
+    num:'04',
+    title:'UX Design',
+    description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, reiciendis!',
+    href:''
+  },
+  {
+    num:'04',
+    title:'Devops',
+    description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, reiciendis!',
+    href:''
+  },
+]
+
 
 function Services() {
   return (
-    <div>Services</div>
+    <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+      <div className='container mx-auto'>Services</div>
+      <motion.div
+        initial={{opacity:0}}
+        animate={{
+            opacity:1,
+            transition:{ delay:2.4, duration:0.4 , ease:"easeIn"},
+        }}
+        className='grid grid-cols-1 md:grid-cols-2 gap-[60px'
+      >
+        {services.map((service, index) => {
+           return( <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
+              <div>
+                <div>{service.num }</div>
+                <Link href={service.href}>
+                  <BsArrowDownRight />
+                </Link>
+              </div>
+              <h2>{service.title}</h2>
+              <p>{service.description}</p>
+           </div>)
+        })}
+
+      </motion.div>
+    </section>
   )
 }
 
