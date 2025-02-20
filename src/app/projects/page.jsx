cecefefa7e62af123654a390b3ee226608c907be
Mohamed <mohamed.warma10@gmail.com>
2025-02-20 14:19:44 +0000

    update of about projects pages

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+'use client'
+
+import React, {useState} from 'react'
+import { motion } from 'framer-motion'
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs' 
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+
+const projects = [ 
+  {
+    num : '01',
+    category: 'full-stack',
+    title: 'restaurant',
+    description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus a neque similique',
+    image : '/',
+    live :'warma-mohamed.onrender.com',
+    github:'github.com/MohamedWARMA',
+    stack:[
+      {
+        name:'Html5'
+      },
+      {
+        name:'Css3'
+      },
+      {
+        name:'Javascript'
+      },
+      {
+        name:'Django'
+      },
+    ]
+  }
+]
 
 function Projects() {
+  const [project, setProject] = useState(projects[0]);
   return (
     <div>Projects</div>
   )
--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -11,7 +11,7 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+      "inline-flex h-auto rounded-md bg-primary p-1 text-black",
       className
     )}
     {...props} />
@@ -22,7 +22,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow",
+      "inline-flex items-center w-full bg-primary justify-center whitespace-nowrap text-black rounded-lg p-3 text-balance font-medium ring-offset-slate-600 transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent data-[state=active]:text-white data-[state=active]:font-bold data-[state=active]:shadow-sm text-base",
       className
     )}
     {...props} />
@@ -33,7 +33,7 @@ const TabsContent = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      "min-h[450px] ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
       className
     )}
     {...props} />
