c5681b9b126292a9e248f70988164c6243797551
Mohamed <mohamed.warma10@gmail.com>
2025-02-21 14:09:39 +0000

    update of MobieNav component

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -13,13 +13,13 @@ function MobileNav() {
                 <CiMenuFries className="text-[32px] text-accent" />
             </SheetTrigger>
             <SheetContent className="flex flex-col">
-                <dv className="mt-2 mb-14 text-center text-2xl">
+                <dv className="mt-2 mb-14 text-center text-xl">
                     <Link href={'/'}>Mohamed<span className="text-accent">.</span></Link>
                 </dv>
 
-                <nav className="flex flex-col justify-center items-center gap-8">
+                <nav className="flex flex-col justify-center items-center gap-4">
                     {mylinks.map((mylink, index) =>{
-                        return <Link href={mylink.path} key={index} className={`${mylink.path === pathname && "text-accent border-b-2 border-accent" } text-xl capitalize hover:text-accent transition-all`}>{mylink.name}</Link>
+                        return <Link href={mylink.path} key={index} className={`${mylink.path === pathname && "text-accent border-b-2 border-accent" } text-[13px] capitalize hover:text-accent transition-all `}>{mylink.name}</Link>
                     })}
                 </nav>
             </SheetContent>
